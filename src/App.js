129de2444a8d864b8eb3b6f0856084e5a9bde2b7
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-03-29 20:11:21 -0300

    task-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Tasks from "./components/Tasks";
 import './App.css';
 const App =()=>{
+  const[tasks,setTasks]=useState([
+    {
+      id:'1',
+      title:'estudar programaçao',
+      complete:false
+    },
+    {
+      id:'2',
+      title:'ler livros',
+      complete:true
+    }
+  ]);
   return(
-    <div className="container"></div>
-  )
+    <>
+      <div className="container">
+        <Tasks tasks={tasks}/>
+      </div>
+    </>
+    )
 }
 
 export default App;
