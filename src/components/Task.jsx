a6caad5891f655d940b9ad0d1ec65b07c7e4af62
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-03-29 23:52:37 -0300

    hitbox-click

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,8 +3,14 @@ import "./Task.css"
 const Task = ({task,handleTaskClick})=>{
     return(
         <>
-            <div className="task-container" style={task.completed ? {borderLeft:"6px solid  chartreuse"} : {} }>
-                {task.title}
+            <div onClick={()=>handleTaskClick(task.id)} className="task-container" style={task.completed ? {borderLeft:"6px solid  chartreuse"} : {} }>
+                <div  className="task-title" onClick={()=>handleTaskClick(task.id)}>
+                    {task.title}
+                </div>
+                <div className="icons-container">
+                    <button className="remove-task-button">X</button>
+                </div>
+                
             </div>
         </>
     )
