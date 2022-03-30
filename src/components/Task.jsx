b1a422ad6a9d6fb91946cbdaded90037fbf92603
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-03-30 00:50:00 -0300

    refactoring removeIcon

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import {CgClose} from "react-icons/cg"
 import "./Task.css"
 const Task = ({task,handleTaskClick,handleTaskDelete})=>{
     return(
         <>
-            <div className="task-container" style={task.completed ? {borderLeft:"6px solid  chartreuse"} : {} }>
+            <div  className="task-container" style={task.completed ? {borderLeft:"6px solid  chartreuse"} : {} }>
                 <div  className="task-title" onClick={()=>handleTaskClick(task.id)}>
                     {task.title}
                 </div>
                 <div className="icons-container">
-                    <button className="remove-task-button" onClick={()=>handleTaskDelete(task.id)}>X</button>
+                    <button className="remove-task-button" onClick={()=>handleTaskDelete(task.id)}>
+                        <CgClose/>
+                    </button>
                 </div>
                 
             </div>
